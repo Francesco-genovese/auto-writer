3a96d11d8935fa9703d181d62a91ddaa48069c9c
R3tr0 <73380860+Francesco-genovese@users.noreply.github.com>
2024-03-08 12:41:30 +0100

    Add files via upload

---
+++ writer.py
@@ -0,0 +1,12 @@
+import pyautogui
+import time
+def scrivi_su_applicazione(messaggio):
+         
+        pyautogui.write(messaggio)
+        pyautogui.press('enter')
+if __name__ == "__main__":
+    messaggio_da_scrivere = "negro basstardo"
+    time.sleep(2)
+    for i in range(100):
+        time.sleep(1)
+        scrivi_su_applicazione(messaggio_da_scrivere)
