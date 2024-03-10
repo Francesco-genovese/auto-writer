ee04af136874c5e04e4e96f1d5ce0cbd8770d186
R3tr0 <73380860+Francesco-genovese@users.noreply.github.com>
2024-03-10 20:24:42 +0100

    Update writer.py

--- writer.py
+++ writer.py
@@ -5,7 +5,7 @@ def scrivi_su_applicazione(messaggio):
         pyautogui.write(messaggio)
         pyautogui.press('enter')
 if __name__ == "__main__":
-    messaggio_da_scrivere = "negro basstardo"
+    messaggio_da_scrivere = "plain text"
     time.sleep(2)
     for i in range(100):
         time.sleep(1)
